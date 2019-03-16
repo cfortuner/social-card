0225d8e1f642b5ab77ccad3ec8977fe76424c18c
Colin Fortuner <cffortun@twitch.tv>
2019-03-16 14:36:03 -0700

    Adding key to list.

--- src/socialcard/component.js
+++ src/socialcard/component.js
@@ -9,25 +9,24 @@ class SocialCard extends React.Component {
   render() {
     const data = {...this.props.data}
 
-    const isDataValid = data && data.profile && data.card
+    const hasData = data && data.profile && data.card
     const hasReactions = data && data.reactions && data.reactions.length > 0 
     return (
-      isDataValid 
-        ? <PageError errorMessage="Data invalid. Missing card information."/>
-        : <>
-          {isDataValid && 
-            <CardHeader className='social-card-header' profile={data.profile} title={data.card.title}/>}
+      hasData
+        ? <>
+            <CardHeader className='social-card-header' profile={data.profile} title={data.card.title}/>
             <CardContent className='social-card-content' media={data}/>
             {hasReactions && 
               <div className='social-card-reactions'>
                 {_.map(data.reactions, (reaction) =>
-                  <li >
+                  <li key={reaction.type}>
                     <Reaction type={reaction.type} value={reaction.value}/>
                   </li>
                 )}
               </div>
             }
           </>
+        : <PageError errorMessage="Data invalid. Missing card information."/>
     )
   }
 }
