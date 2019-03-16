f9a93b4191e69a8c2a67f4c9981c3f0369aa174b
Colin Fortuner <cffortun@twitch.tv>
2019-03-16 14:29:54 -0700

    Updating social card component with data and passing through props.

--- src/socialcard/component.js
+++ src/socialcard/component.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import CardContent from './components/cardcontent/component';
-import CardHeader from './components/cardheader/component';
-import Reaction from '../features/reaction/component';
+import React from 'react'
+import CardContent from './components/cardcontent/component'
+import CardHeader from './components/cardheader/component'
+import Reaction from '../features/reaction/component'
+import PageError from '../features/error/component'
+import _ from 'lodash'
 
 class SocialCard extends React.Component {
   render() {
-    return (
-      <div>
-        <CardHeader/>
-        <CardContent/>
-        <div className='social-card-reactions'>
-          <Reaction></Reaction>
-        </div>
-      </div>
+    const data = {...this.props.data}
 
+    const isDataValid = data && data.profile && data.card
+    const hasReactions = data && data.reactions && data.reactions.length > 0 
+    return (
+      isDataValid 
+        ? <PageError errorMessage="Data invalid. Missing card information."/>
+        : <>
+          {isDataValid && 
+            <CardHeader className='social-card-header' profile={data.profile} title={data.card.title}/>}
+            <CardContent className='social-card-content' media={data}/>
+            {hasReactions && 
+              <div className='social-card-reactions'>
+                {_.map(data.reactions, (reaction) =>
+                  <li >
+                    <Reaction type={reaction.type} value={reaction.value}/>
+                  </li>
+                )}
+              </div>
+            }
+          </>
     )
   }
 }
 
-export default SocialCard;
+export default SocialCard
