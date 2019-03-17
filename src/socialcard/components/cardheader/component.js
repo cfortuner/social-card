32f44317a8b71764b7efbc1c5ba14321af68deed
Colin Fortuner <cffortun@twitch.tv>
2019-03-17 13:16:40 -0700

    Adding styles to social card header.

--- src/socialcard/components/cardheader/component.js
+++ src/socialcard/components/cardheader/component.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from 'react'
+import './styles.scss'
 
 class CardHeader extends React.Component {
   render() {
     const { profile, date, title} = {...this.props}
     return (
-      <div>
+      <div className='social-card-header'>
         <b>{profile.displayName}</b> {" "}<i>{"@" + profile.handle + " - " + date}</i>
         <div className="title">
           {title}
