1874b9867bbc0c946c2b646ba3ec2d38cde9e97a
Colin Fortuner <cffortun@twitch.tv>
2019-03-17 11:04:58 -0700

    Replacing error page component with generic error.

--- src/socialcard/component.js
+++ src/socialcard/component.js
@@ -2,7 +2,6 @@ import React from 'react'
 import CardContent from './components/cardcontent/component'
 import CardHeader from './components/cardheader/component'
 import Reaction from '../features/reaction/component'
-import PageError from '../features/error/component'
 import _ from 'lodash'
 
 class SocialCard extends React.Component {
@@ -10,23 +9,24 @@ class SocialCard extends React.Component {
     const data = {...this.props.data}
 
     const hasData = data && data.profile && data.card
-    const hasReactions = data && data.reactions && data.reactions.length > 0 
+    const hasReactions = data && data.reactions && data.reactions.length > 0
+
+    if (!hasData) throw new Error('Error! Social Card data missing from props.')
+
     return (
-      hasData
-        ? <>
-            <CardHeader className='social-card-header' profile={data.profile} card={data.card}/>
-            <CardContent className='social-card-content' media={data}/>
-            {hasReactions && 
-              <div className='social-card-reactions'>
-                {_.map(data.reactions, (reaction) =>
-                  <li key={reaction.type}>
-                    <Reaction type={reaction.type} value={reaction.value}/>
-                  </li>
-                )}
-              </div>
-            }
-          </>
-        : <PageError errorMessage="Data invalid. Missing card information."/>
+      <>
+        <CardHeader className='social-card-header' profile={data.profile} card={data.card}/>
+        <CardContent className='social-card-content' media={data}/>
+        {hasReactions &&
+          <div className='social-card-reactions'>
+            {_.map(data.reactions, (reaction) =>
+              <li key={reaction.type}>
+                <Reaction type={reaction.type} value={reaction.value}/>
+              </li>
+            )}
+          </div>
+        }
+      </>
     )
   }
 }
