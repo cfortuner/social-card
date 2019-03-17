d0267873863316641d437b51b3bdfd118a732551
Colin Fortuner <cffortun@twitch.tv>
2019-03-17 13:18:08 -0700

    Adding static card content.

--- src/socialcard/components/cardcontent/component.js
+++ src/socialcard/components/cardcontent/component.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from 'react'
+import './styles.scss'
 
 class CardContent extends React.Component {
   render() {
     return (
-      <div>
+      <div className='social-card-content'>
+        <img src={window.location.origin + this.props.card.media} alt='Card Media'/>
+        <div className='social-card-content-body'>
+          <h3>{this.props.card.title}</h3>
+          <p>{this.props.card.message}</p>
+          <p><i style={{color:'grey'}}>{this.props.card.link}</i></p>
+        </div>
       </div>
-
     )
   }
 }
