8f0555030f3431287eb247d16bc68341eec102dc
Colin Fortuner <cffortun@twitch.tv>
2019-05-17 10:10:35 -0700

    Adding reactions.

--- src/features/reaction/component.js
+++ src/features/reaction/component.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 class Reaction extends React.Component {
+
+  icons = {
+    "comments": "/img/comments-icon.png",
+    "reshares": "/img/reshares-icon.png",
+    "loves": "/img/loves-icon.png",
+    "email": "/img/email-icon.png",
+  }
+
   render() {
     return (
       <div>
+        <img src={window.location.origin + this.icons[this.props.type]} alt={"reaction"}/>
+        {this.props.value}
       </div>
-
     )
   }
+
+
 }
 
 export default Reaction;
